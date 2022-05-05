58f0dfe39e4cec2d4c7f4ca50726a0094b4b5fde
Sobolev Maksim <maxim.sobolev@hrb.software>
2022-05-05 14:46:20 +0600

    fix cors

--- src/main.js
+++ src/main.js
@@ -7,13 +7,15 @@ const {dbUrl, mongoOptions} = require('../config')
 const auth = require('../src/routes/auth');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
 mongoose.connect(dbUrl, mongoOptions, () => {
   app.use('/auth', auth)
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 8000;
 
   app.listen(PORT, () => {
     console.log(`app started on ${PORT}`)
